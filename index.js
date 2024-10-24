9446424ef26169036b0bafcb0530e9bda17b7ccf
Shanuka Lakshan <89212510+ShanukaLakshan@users.noreply.github.com>
2024-10-24 21:23:47 +0530

    Refactor API welcome message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the Learnaware API with Jenkins!");
+  res.send("Welcome to the Learnaware API!");
 });
 
 app.get("/api/users", (req, res) => {
